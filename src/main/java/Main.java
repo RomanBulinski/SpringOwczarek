4569ef66a8b681e036a8a80aac14643d708a054c
Roman Bulinski <aboandrus@gmail.com>
2019-03-10 11:35:20 +0100

    Implementation without Spring

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import implementation.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ILogger logger = new Logger();
+        IUsersRepository usersRepository = new UserRepository();
+        usersRepository.setLogger((Logger) logger);
+
+        User user = usersRepository.createUser("Darek");
+
+    }
+
+}
